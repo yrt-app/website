a6df337e73bb467364f4a33976a751d30ea56e72
lorenzsimon <lorenz.simon.mail@gmail.com>
2024-04-07 19:16:30 +0200

    pre-render pages

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,6 +6,6 @@ import cloudflare from "@astrojs/cloudflare";
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind()],
-  output: "server",
-  adapter: cloudflare()
+  output: "hybrid",
+  adapter: cloudflare(),
 });
